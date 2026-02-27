585666f312598bb82a2365f93624b1bd201f2b38
Rafael Miguel Madrigal Lamadrid <lrafaelmadrigal@gmail.com>
2026-02-27 19:59:08 +0100

    Listo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,12 +1,12 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-20">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center py-2">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-8 sm:h-9 w-auto" />
+                        <x-application-mark class="block h-12 sm:h-14 w-auto" />
                     </a>
                 </div>
 
